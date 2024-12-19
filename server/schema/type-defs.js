b7a4c43f2bcecafa28b4da735a9abe0213b2e733
PreethikhaS <preethikhasundarabharathy@gmail.com>
2024-12-19 08:41:42 -0800

    updated files

--- server/schema/type-defs.js
+++ server/schema/type-defs.js
@@ -4,16 +4,25 @@ const { gql } = require('apollo-server'); // gql is a variable this allows us to
 //every graphql scheme will start with one specific type, thats called query
 // we wrap [] around to get a list of such field
 //accordingly we need to have a resolver for the Query, here for the users field
+//Here Nationality is an enum (its a best practice to have enum in capital letters)
 const typeDefs = gql`
     type User{
         id:ID!
         name: String!
         username: String!
         age: Int!
-        nationality:String!
+        nationality:Nationality!
+        friends:[User]
     }
     type Query{
-        users: [User!]
+        users: [User!]!
+    }
+
+    enum Nationality{
+        CANADA
+        BRAZILINDIA
+        GERMANY
+        CHILE
     }
 `;
 
